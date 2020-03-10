25c6938b5e3a5c923aaf5b80a9039ee3382137ef
tengfei.dai <fxt17693636789>
2020-03-10 21:47:18 +0800

    Initial commit

---
+++ src/main/java/com/fei/test/LombokTest.java
@@ -0,0 +1,25 @@
+package com.fei.test;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @ProjectName: lombok
+ * @Package: com.fei.test
+ * @ClassName: LombokTest
+ * @Author: feixiaoteng
+ * @Date: 2020-03-10 21:17
+ * @Version: 1.0.0
+ * @Description:
+ */
+@Slf4j
+public class LombokTest {
+    /*private static final Logger logger = LoggerFactory.getLogger(LombokTest.class);
+    public static void main(String[] args) {
+        logger.debug("这是一个日志:{}","hahah");
+    }*/
+    public static void main(String[] args) {
+    }
+}
